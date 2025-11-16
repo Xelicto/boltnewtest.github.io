24f949fe9e3e7c77885e1dd86bef96aaa44a89d9
Xelicto <120069833+Xelicto@users.noreply.github.com>
2025-11-16 14:52:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 💡 ADD THIS LINE 💡
+  // This ensures that all asset paths (like your main.tsx script)
+  // are generated relative to the root of the deployed domain.
+  base: '/',
+  
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
